eeec2febc91224e3f9db3c98377d1eaa16c75d8f
DaulB <41420800+DaulB@users.noreply.github.com>
2018-07-20 10:56:06 -0700

    Update: in /.about now lets you upload a file

--- ng5/src/app/about/about.component.ts
+++ ng5/src/app/about/about.component.ts
@@ -1,15 +1,62 @@
 import { Component, OnInit } from '@angular/core';
+import {trigger,style,transition,animate,keyframes,query,stagger} from '@angular/animations';
+import { NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
+  animations: [
+
+    trigger('goals', [
+      transition('* => *', [
+        query(':enter',style({opacity: 0}), {optional: true}),
+
+        query(':enter', stagger('300ms', [
+          animate('.6s ease-in', keyframes([
+            style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
+            style({opacity: .5, transform: 'translateY(35px)', offset: .3}),
+            style({opacity: 1, transform: 'translateY(0)', offset: 1}),
+          ]))]), {optional: true}),
+
+        query(':leave', stagger('100ms', [
+          animate('.3s ease-in', keyframes([
+            style({opacity: 1, transform: 'translateY(0)', offset: 0}),
+            style({opacity: .5, transform: 'translateY(15px)', offset: .3}),
+            style({opacity: 0, transform: 'translateY(-20%)', offset: 1}),
+          ]))]), {optional:true})
+
+      ])
+    ])
+  ]
 })
+
 export class AboutComponent implements OnInit {
+
+  itemCount: number = 4;
+  btnText: string = "Add an item";
+  goalText: string = 'Select .json file from library...';
+  goals = ['Skydiving', 'Swimming with Sharks', 'Dancing Naked on Ice'];
+  validfile: File;
 
   constructor() { }
 
   ngOnInit() {
+    this.itemCount = this.goals.length;
   }
 
+  addItem(){
+    this.goals.push(this.goalText);
+    this.goalText= '';
+    this.itemCount = this.goals.length;
+  }
+
+  removeItem(i){
+    this.goals.splice(i, 1);
+  }
+
+  validFile(file){
+    //once the file is uploaded we have to check whether it fits the pattern for War or Tra.
+
+  }
 }
